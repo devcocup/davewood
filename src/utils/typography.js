78526de74ce73193d7f64577ca6f3140f00bef2e
typescript59 <typescript59@gmail.com>
2018-09-30 19:40:26 -0500

    Update README explanation and typography themes

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,8 +1,10 @@
 import Typography from "typography";
 //import bootstrapTheme from "typography-theme-bootstrap";
-import irvingTheme from "typography-theme-irving";
+//import irvingTheme from "typography-theme-irving";
 import judahTheme from "typography-theme-judah";
 
 const typography = new Typography(judahTheme);
+//const typography = new Typography(bootstrapTheme);
+//const typography = new Typography(irvingTheme);
 
 export default typography;
