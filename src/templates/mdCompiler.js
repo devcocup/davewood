2ce5aad80118d70b0801dd3a7a6917a178d27fde
typescript59 <typescript59@gmail.com>
2018-11-22 16:53:08 -0600

    Import index.css

--- src/templates/mdCompiler.js
+++ src/templates/mdCompiler.js
@@ -1,6 +1,7 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { graphql } from "gatsby";
+import "../pages/index.css"
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Sidebar from "../components/Sidebar";
